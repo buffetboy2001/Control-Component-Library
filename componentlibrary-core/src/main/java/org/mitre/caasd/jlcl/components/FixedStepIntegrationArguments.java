b0c8c7bd3b53a412a1abb5a1d1f5ee1cb0b4eda1
sbowman <sbowman@mitre.org>
2014-09-08 21:57:07 -0400

    comment tweaks only

--- componentlibrary-core/src/main/java/org/mitre/caasd/jlcl/components/FixedStepIntegrationArguments.java
+++ componentlibrary-core/src/main/java/org/mitre/caasd/jlcl/components/FixedStepIntegrationArguments.java
@@ -6,18 +6,20 @@ package org.mitre.caasd.jlcl.components;
 import org.mitre.caasd.jlcl.interfaces.IFixedStepIntegratorArguments;
 
 /**
- * This class does not contain a setter, so the value cannot be updated within the lifetime of the object.
+ * This class does not contain a setter, so the value cannot be updated within
+ * the lifetime of the object.
  * 
  * @author SBOWMAN
  * 
  */
-public class FixedStepIntegrationArguments<NUMERICTYPE extends Number> extends FixedStepIterativeAlgorithmArguments<NUMERICTYPE> implements IFixedStepIntegratorArguments<NUMERICTYPE> {
+public class FixedStepIntegrationArguments<NUMERICTYPE extends Number> extends FixedStepIterativeAlgorithmArguments<NUMERICTYPE> implements
+        IFixedStepIntegratorArguments<NUMERICTYPE> {
 
     /**
      * Constructor.
      */
     public FixedStepIntegrationArguments(Class<NUMERICTYPE> clazz, NUMERICTYPE fixedStepSize) {
-        super(clazz,fixedStepSize);
+        super(clazz, fixedStepSize);
     }
 
 }
